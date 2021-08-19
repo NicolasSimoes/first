9a9b0db3bc1af40554d95346614735b7f90a2b14
NicolasSimoes <89150200+NicolasSimoes@users.noreply.github.com>
2021-08-19 15:28:59 -0300

    Update Principal.java

--- Principal.java
+++ Principal.java
@@ -6,27 +6,27 @@ public class Principal {
        // Class2 class2 = new Class2();
        // class2.contagem();
 
-      //  Class3 class3 = new Class3();
+        // Class3 class3 = new Class3();
        // class3.somaNumeros();
 
-       // Class4 class4 = new Class4();
-       // class4.fatorial();
+        // Class4 class4 = new Class4();
+        // class4.fatorial();
 
-       // Class5 class5 = new Class5();
-       // class5.serieInfinita();
+        // Class5 class5 = new Class5();
+        // class5.serieInfinita();
 
         //Class6 class6 = new Class6();
-       // class6.fibonacci();
+        // class6.fibonacci();
 
-   // Class7 class7 = new Class7();
-    // class7.inversaoNum();
+        // Class7 class7 = new Class7();
+        // class7.inversaoNum();
 
-     //  Class8 class8 = new Class8();
-       //class8.binario();
+        //  Class8 class8 = new Class8();
+        //class8.binario();
 
 
-        //Class9 class9 = new Class9();
-       // class9.caracNum();
+         //Class9 class9 = new Class9();
+        // class9.caracNum();
 
 
 
